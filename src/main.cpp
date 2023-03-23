a687936a5909ffe51188a39f1a730227114c77c1
danielei9 <dbs99@hotmail.com>
2023-03-23 10:22:34 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
 //
 //This example creates a bridge between Serial and Classical Bluetooth (SPP)
 //and also demonstrate that SerialBT have the same functionalities of a normal Serial
-
+static uint8_t DEVEUI_DEF[8]= { 0x17, 0x1f, 0xe2, 0x1d, 0x71, 0xB3, 0x03, 0x54 }  ; // 5403b3711de21f17 139971e1a31d4764
+static uint8_t APPKEY_DEF[16] = {0x06, 0x26, 0x35, 0x77, 0xC3, 0xBB, 0xC9, 0xEB, 0x2F, 0xEF, 0x99, 0x13, 0x5E, 0xF0, 0xF6, 0xA5};
+int TIME_TX = 30000; // ms 
 
 #include "BluetoothSerial.h"
 
@@ -23,7 +25,16 @@ void setup() {
 
 void scheduleRXMessage(String s){
   if( s.startsWith("sAA=")){
-    SerialBT.println("B0_APPKEY_DEVEUI_TIME");
+    SerialBT.print("A0_");
+    //TODO: Convertir a string como : 06263577C3BBC9EB2FEF99135EF0F6A5
+    // SerialBT.print(APPKEY_DEF);
+    SerialBT.print("_");
+    //TODO: Convertir a string como : 171fe21d71B30354
+    // SerialBT.print(DEVEUI_DEF);
+    SerialBT.print("_");
+    //TODO: Convertir a string como : 30000
+    SerialBT.print(TIME_TX);
+    SerialBT.print("_");
     Serial.println("B0_APPKEY_DEVEUI_TIME");
   }
 
